e49fd8a331f17ee4dd490515a844b6d54e819449
Simon <simonlerouxnlp@gmail.com>
2019-05-07 22:34:30 -0400

    Refactored navbar for better readability, still need to fix sub-menu display but now full responsive !

--- frontend/navbar/reactnavbar.jsx
+++ frontend/navbar/reactnavbar.jsx
@@ -1,62 +1,60 @@
+class Links extends React.Component {
+	render() {
+		return (
+			<div>
 
-import React from "react"
+				<a href="#">NUTRITION FINE FOURCHETTE</a>
+				<a href="#" onClick={this.menuToggle} >MENU</a>
+				<a href="#">ORDER</a>
 
-const Navbar = React.createClass({
+				<a href="#">DELIVERY</a>
+				<a href="#">ABOUT</a>
+				<a href="#">LOGIN</a>
+				<a href="#"><i className="fa fa-search fa-2x"></i></a>
+			</div>
+		);
+	}
+}
 
-	// render function for mainnavbar
-	render: function () {
+class Navbar extends React.Component {
+	render() {
 		return (
 			<nav>
+
 				<div className="navWide">
 					<div className="wideDiv">
-						<a className="leftnav" href="#">LOGO</a>
-						<a className="leftnav" href="#">NUTRITION FINE            FOURCHETTE</a>
-						<a className="links" href="#" onClick={this.menuToggle}>MENU</a>
-						<a className="links" href="#">ORDER</a>
-						<a className="links" href="#">DELIVERY</a>
-						<a className="links" href="#">ABOUT</a>
-						<a className="links" href="#">LOGIN</a>
-						<a className="links" href="#">
-							<i className="fa fa-search fa-2x"></i>
-						</a>
-
+						<Links />
 					</div>
 				</div>
-
-				<div className="navNarrow">
-					<p>LOGO</p>
-					<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
-					<div className="narrowLinks">
-						<a href="#" onClick={this.burgerToggle}>MENU</a>
-						<a href="#" onClick={this.burgerToggle}>ORDER</a>
-						<a href="#" onClick={this.burgerToggle}>LOGIN</a>
-						<a href="#" onClick={this.burgerToggle}>SEARCH</a>
-					</div>
-				</div>
-				<div className="menus">
+				<div className="menus"
+					onClick={this.menuToggle}>
 					<a href="#" onClick={this.menuToggle}>WEEKLY</a>
 					<a href="#" onClick={this.menuToggle}>FITNESS</a>
 					<a href="#" onClick={this.menuToggle}>COOKED</a>
 				</div>
+				<div className="navNarrow" onClick={this.toggle}>
+					<span> <i className="fas fa-hamburger fa-2x"></i></span>
+
+					<i className="fa fa-bars fa-2x" />
+					<div className="narrowLinks hidden">
+						<Links />
+					</div>
+				</div>
 			</nav>
 		);
-	},
-	burgerToggle: function () {
-		let linksEl = document.querySelector('.narrowLinks');
-		if (linksEl.style.display === 'block') {
-			linksEl.style.display = 'none';
-		} else {
-			linksEl.style.display = 'block';
-		}
-	},
-	menuToggle: function () {
-		let menus = document.querySelector('.menus');
-		if (menus.style.display === 'inline-block') {
-			menus.style.display = 'none';
-		} else {
-			menus.style.display = 'inline-block';
-		}
 	}
-});
 
-export default Navbar
+	toggle() {
+		let narrowLinks = document.querySelector(".narrowLinks");
+		narrowLinks.classList.toggle("hidden");
+	}
+	toggleMenu() {
+		let menu = document.querySelector(".menus")
+		menu.style.display = "inline-block"
+	}
+
+
+
+}
+
+ReactDOM.render(<Navbar />, document.getElementById("root"));
