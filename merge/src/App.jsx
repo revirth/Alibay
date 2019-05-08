27e480984f17bbb8724382af0fbbc2700dfb575c
iZsk <sk.zeeshan1992@gmail.com>
2019-05-08 15:48:20 -0400

    final update for item list and details

--- merge/src/App.jsx
+++ merge/src/App.jsx
@@ -6,6 +6,18 @@ import Cart from "./component/cartfooter/Cart";
 import CartBar from "./component/cartfooter/CartBar";
 import itemList from "./component/itemlist/index";
 import Navbar from "./component/navbar/reactnavbar";
+import Itempage from "./component/itemlist/itempage.jsx";
+
+let renderItem = routerData => {
+  // 10
+
+  // 11
+  return (
+    <div>
+      <Itempage id={routerData.match.params.itemId} />
+    </div>
+  ); // 11
+}; // 11
 
 function App() {
   return (
@@ -14,6 +26,7 @@ function App() {
       <div>
         <Route exact={true} path="/items" component={itemList} />
         <Route exact={true} path="/cart" component={Cart} />
+        <Route exact={true} path="/items/:itemId" render={renderItem} />
       </div>
       <CartBar />
     </BrowserRouter>
--- merge/src/component/itemlist/product.jsx
+++ merge/src/component/itemlist/product.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import "./main.css";
+import "./style.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const Product = ({ id, name, description, imgUrl, price, addFunc }) => {
+const Product = ({ _id, name, description, imgUrl, price, addFunc }) => {
   return (
     <article
       className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center"
-      id={id}
+      id={_id}
     >
       <img
         src={imgUrl}
         className="db w-100 br2 br--top"
         alt="Photo of a kitten looking menacing."
         title={name}
+        height="200px"
+        width="100px"
       />
       <div className="pa2 ph3-ns pb3-ns">
         <div className="dt w-100 mt1">
@@ -24,14 +29,18 @@ const Product = ({ id, name, description, imgUrl, price, addFunc }) => {
         <p className="f6 lh-copy measure mt2 mid-gray">{description}</p>
         <div className="btn">
           <button
-            className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn"
+            className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn grow"
             onClick={() => addFunc()}
           >
             Add to cart
           </button>
-          <button className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn">
-            Customize{" "}
-          </button>
+
+          <Link
+            className="f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn grow"
+            to={`/items/${_id}`}
+          >
+            More Details
+          </Link>
         </div>
       </div>
     </article>
