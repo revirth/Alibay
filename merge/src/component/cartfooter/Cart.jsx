2ca64f2a5ae1d910385764ed4b48f6b4ead85379
iZsk <sk.zeeshan1992@gmail.com>
2019-05-08 16:46:57 -0400

    changed color in footer section

--- merge/src/component/cartfooter/Cart.jsx
+++ merge/src/component/cartfooter/Cart.jsx
@@ -1,55 +1,96 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import './cart.css'
+import React from "react";
+import { connect } from "react-redux";
+import "./cart.css";
+import "./style.css";
 
-class UnConnectedCart extends React.Component{
+class UnConnectedCart extends React.Component {
+  onChangeHandleQuantity = e => {
+    this.props.dispatch({
+      type: "ChangeQuantity",
+      itemId: e.target.id,
+      quantity: e.target.value
+    });
+  };
 
-    onChangeHandleQuantity = (e) => {
-        this.props.dispatch({type: "ChangeQuantity", itemId: e.target.id, quantity: e.target.value})
-    }
+  onClickRemoveItem = e => {
+    this.props.dispatch({ type: "RemoveItem", itemId: parseInt(e.target.id) });
+  };
 
-    onClickRemoveItem = (e) => {
-        this.props.dispatch({type: "RemoveItem", itemId: parseInt(e.target.id)})
-    }
-
-    render(){
-    let total = 0
-    this.props.items.forEach( item => {
-        total = total + parseFloat(item.itemPrice)*parseInt(item.itemQuantity)
-    })
-    return(<div className="general-margin">
+  render() {
+    let total = 0;
+    this.props.items.forEach(item => {
+      total = total + parseFloat(item.itemPrice) * parseInt(item.itemQuantity);
+    });
+    return (
+      <div className="general-margin">
         <h4>Your Items:</h4>
-        {this.props.items.map( item => {
-            return (<div className="item-cell-width">
-                <div className="item-in-column">
-                <div className="image"><img src={"/"+item.itemImage} height = "100px" alt=""></img></div>
+        {this.props.items.map(item => {
+          return (
+            <div className="item-cell-width">
+              <div className="item-in-column">
+                <div className="image">
+                  <img src={"/" + item.itemImage} height="100px" alt="" />
+                </div>
                 <div className="information-in-row">
-                    <div>
+                  <div>
                     <div>Name: {item.itemName}</div>
-                    <hr></hr>
+                    <hr />
                     <div className="stick_bottom">${item.itemPrice}</div>
+                  </div>
+                  <div className="parent">
+                    <div className="stick_bottom">
+                      Qt:{" "}
+                      <input
+                        className="input-number"
+                        type="number"
+                        value={item.itemQuantity}
+                        id={item.itemId}
+                        onChange={this.onChangeHandleQuantity}
+                      />
                     </div>
-                    <div className="parent"><div className="stick_bottom">Qt: <input className="input-number" type="number" value={item.itemQuantity} id={item.itemId} onChange={this.onChangeHandleQuantity}></input></div></div>
+                  </div>
                 </div>
-                <div className="parent"><div className="stick_bottom subtotal">Subtotal: {(parseFloat(item.itemPrice)*item.itemQuantity).toFixed(2)}</div></div>
-                <div><i className="fa fa-times" id={item.itemId} onClick={this.onClickRemoveItem}></i></div>
+                <div className="parent">
+                  <div className="stick_bottom subtotal">
+                    Subtotal:{" "}
+                    {(parseFloat(item.itemPrice) * item.itemQuantity).toFixed(
+                      2
+                    )}
+                  </div>
                 </div>
-                <hr></hr>
-            </div>)
+                <div>
+                  <i
+                    className="fa fa-times"
+                    id={item.itemId}
+                    onClick={this.onClickRemoveItem}
+                  />
+                </div>
+              </div>
+              <hr />
+            </div>
+          );
         })}
-    <div className="total">Total: {total.toFixed(2)}</div>
-    <div className="parent-horizontal">
-    <div classname="button-right"><button className="checkout-button" onClick={this.onClickHandle}>Checkout</button></div>
-    </div>
-    </div>)
-        }
+        <div className="total">Total: {total.toFixed(2)}</div>
+        <div className="parent-horizontal">
+          <div classname="button-right">
+            <button
+              className="checkout-button f6 link dim br3 ph3 pv2 mb2 dib white bg-dark-green bn grow"
+              onClick={this.onClickHandle}
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-let mapStateToProps = (state) => {
-    console.log(state.items)
-    return {items: state.items}
-}
+let mapStateToProps = state => {
+  console.log(state.items);
+  return { items: state.items };
+};
 
-let Cart = connect(mapStateToProps)(UnConnectedCart)
+let Cart = connect(mapStateToProps)(UnConnectedCart);
 
-export default Cart
+export default Cart;
--- merge/src/component/cartfooter/CartBar.jsx
+++ merge/src/component/cartfooter/CartBar.jsx
@@ -1,41 +1,58 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import './cartBar.css'
-
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import "./cartBar.css";
+import "./style.css";
 class UnconnectedCartBar extends React.Component {
-
-    render(){
-        return(
-            <div className="footer row">
-                <div className="equal-width"></div>
-                <div className="equal-width">
-                        <div className="column text-title">Cart :</div>
-                        <div className="column number"> {this.props.number}</div>
-                        <div className="column">
-                            <div className="height100"><img src="https://image.flaticon.com/icons/svg/60/60992.svg" height='15px' alt=""></img></div>
-                            <div className="text">items</div>
-                        </div>
-                </div>
-                <div className="column equal-width"><div className="column text-title">Total: </div><div className="column number">${this.props.total.toFixed(2)}</div></div>
-                <div className="column equal-width"><Link to="/cart/"><button className="view-cart-button" onClick={this.onClickHandle}>View Cart</button></Link></div>
-                <div className="equal-width"></div>
-            </div>)
-    }
-
+  render() {
+    return (
+      <div className="footer row">
+        <div className="equal-width" />
+        <div className="equal-width">
+          <div className="column text-title">Cart :</div>
+          <div className="column number"> {this.props.number}</div>
+          <div className="column">
+            <div className="height100">
+              <img
+                src="https://image.flaticon.com/icons/svg/60/60992.svg"
+                height="15px"
+                alt=""
+              />
+            </div>
+            <div className="text">items</div>
+          </div>
+        </div>
+        <div className="column equal-width">
+          <div className="column text-title">Total: </div>
+          <div className="column number">${this.props.total.toFixed(2)}</div>
+        </div>
+        <div className="column equal-width">
+          <Link to="/cart/">
+            <button
+              className="view-cart-button f6 link dim br3 ph3 pv2 mb2 dib white  bn grow"
+              onClick={this.onClickHandle}
+            >
+              View Cart
+            </button>
+          </Link>
+        </div>
+        <div className="equal-width" />
+      </div>
+    );
+  }
 }
 
-let mapStatetoProps = (state) =>{
-    let numberOfItems = 0
-    let totalPrice = 0
-    state.items.forEach( item => {
-        totalPrice = totalPrice + parseFloat(item.itemPrice)*parseInt(item.itemQuantity)
-        numberOfItems = numberOfItems + parseInt(item.itemQuantity)
-    })
-    return ({number: numberOfItems, total: totalPrice })
+let mapStatetoProps = state => {
+  let numberOfItems = 0;
+  let totalPrice = 0;
+  state.items.forEach(item => {
+    totalPrice =
+      totalPrice + parseFloat(item.itemPrice) * parseInt(item.itemQuantity);
+    numberOfItems = numberOfItems + parseInt(item.itemQuantity);
+  });
+  return { number: numberOfItems, total: totalPrice };
+};
 
-}
+let CartBar = connect(mapStatetoProps)(UnconnectedCartBar);
 
-let CartBar = connect(mapStatetoProps)(UnconnectedCartBar)
-
-export default CartBar
+export default CartBar;
