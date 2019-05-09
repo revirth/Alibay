0e808cdc95dd618952a4ed2e48106022b1402007
iZsk <sk.zeeshan1992@gmail.com>
2019-05-09 17:24:27 -0400

    minor update

--- merge/src/component/navbar/reactnavbar.jsx
+++ merge/src/component/navbar/reactnavbar.jsx
@@ -1,8 +1,62 @@
 import React from "react";
 import "./reactnavbar.scss";
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
+import { connect } from "react-redux";
+
+// let Links = (props) => {
+
+//   const loginOrProfile = (login) => {
+//     // ternary operator statement ? (if true) : (if false)
+//     return auth.isAuthenticated ?
+//       // navbar if user is login= true
+//       <div className="navbarmains">
+//         <a href="/" className="titles">
+//           Nutrition Fine Fourchette
+//         </a>
+//         <a href="#" onClick={this.menuToggle}>
+//           MENU
+//         </a>
+//         <a href="/cart">ORDER</a>
+
+//         <a href="#">DELIVERY</a>
+//         <a href="/">ABOUT</a>
+//         <a href="/">{this.props.username}</a>
+//         <a href="/logout">LOGOUT</a>
+
+//         <i id="searchbutton" className="fa fa-search fa" />
+//         <input />
+//       </div>
+
+//       :
+
+//       // navbar if user is login=false
+//       <div className="navbarmains">
+//         <a href="/" className="titles">
+//           Nutrition Fine Fourchette
+//         </a>
+//         <a href="#" onClick={this.menuToggle}>
+//           MENU
+//         </a>
+//         <a href="/cart">ORDER</a>
+
+//         <a href="#">DELIVERY</a>
+//         <a href="/">ABOUT</a>
+//         <a href="/login">LOGIN</a>
+//         <a href="/login">SIGNUP</a>
+//         <i id="searchbutton" className="fa fa-search fa" />
+//         <input />
+//       </div>
+//   };
 
 class Links extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     username : "" ,
+  //     login : false
+  //   }
+  // }
+
   menuToggle() {
     let menus = document.querySelector(".menus");
     if (menus.style.display === "inline-block") {
@@ -15,7 +69,9 @@ class Links extends React.Component {
   render() {
     return (
       <div className="navbarmains">
-        <a href="/" className="titles">Nutrition Fine Fourchette</a>
+        <a href="/" className="titles">
+          Nutrition Fine Fourchette
+        </a>
         <a href="#" onClick={this.menuToggle}>
           MENU
         </a>
@@ -26,32 +82,44 @@ class Links extends React.Component {
         <a href="/login">LOGIN</a>
         <a href="/login">SIGNUP</a>
         <i id="searchbutton" className="fa fa-search fa" />
-        <input></input>
+        <input />
       </div>
     );
   }
 }
 
+// let mapStateToProps = {
+//   return {
+//     username : state.username,
+//     login : state.login
+//   }
+// };
+
+// let Link = connect(mapStateToProps)(UnconnectedLinks)
+
 export default class Navbar extends React.Component {
   render() {
     return (
+      //main navbar selectors + links component based on if user is log or no
       <nav className="fixednav">
         <div className="navWide">
           <div className="wideDiv">
+            {/* {loginOrProfile(props.login)} */}
             <Links />
           </div>
         </div>
+
         <Fade top>
           <div className="menus" onClick={this.menuToggle}>
             <a href="/items" onClick={this.menuToggle} onClick={this.toggle}>
               WEEKLY
-          </a>
+            </a>
             <a href="/items" onClick={this.menuToggle} onClick={this.toggle}>
               FITNESS
-          </a>
+            </a>
             <a href="/items" onClick={this.menuToggle} onClick={this.toggle}>
               COOKED
-          </a>
+            </a>
           </div>
         </Fade>
         <div className="navNarrow" onClick={this.toggle}>
@@ -82,5 +150,3 @@ export default class Navbar extends React.Component {
     }
   }
 }
-
-// ReactDOM.render(<Navbar />, document.getElementById("root"));
--- merge/src/store.js
+++ merge/src/store.js
@@ -1,29 +1,39 @@
-import { createStore } from "redux";
+import {
+  createStore
+} from "redux";
 
 let reducer = (state, action) => {
   if (action.type === "ChangeQuantity") {
     return {
       ...state,
       cartItems: state.cartItems.map(item => {
-        if(item.itemId === action.itemId){
-          return { ...item, itemQuantity: parseInt(action.quantity) }
-                                                   } else{
+        if (item.itemId === action.itemId) {
+          return {
+            ...item,
+            itemQuantity: parseInt(action.quantity)
+          }
+        } else {
           return item
-                                                         }
-                                             }
-      )
+        }
+      })
     };
   }
 
   if (action.type === "RemoveItem") {
-    let newItems = state.cartItems.filter( item => {
+    let newItems = state.cartItems.filter(item => {
       return item.itemId !== action.itemId
     })
-    return {...state, cartItems: newItems}
+    return {
+      ...state,
+      cartItems: newItems
+    }
   }
 
-  if(action.type === "FillCart") {
-    return {...state, cartItems: action.cartItems}
+  if (action.type === "FillCart") {
+    return {
+      ...state,
+      cartItems: action.cartItems
+    }
   }
 
   return state;
@@ -32,9 +42,10 @@ let reducer = (state, action) => {
 
 
 let store = createStore(
-  reducer,
-  {
-    cartItems: []
+  reducer, {
+    cartItems: [],
+    login: false,
+    username: ""
   },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
